e732b1afdde194292711f8a823a7b0b2ec726a00
Harrison <koharrisonko@gmail.com>
2023-02-06 22:01:33 -0600

    implemented navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,35 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    //py-8 represents y padding, relative is position according to normall flow and offsets are calculated
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    
+    <div /*inset covers whole element, absolute width makes the div smaller*/className="absolute w-[50%] inset-0 gradient-01" />
+    <div /*makes the div go all the way through, centers element, makes items inside a list, make equal space,make gap*/className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="search"
+        //object-contain makes the thing contain insuide
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">ANGELINA</h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;
--- styles/index.js
+++ styles/index.js
@@ -1,9 +1,11 @@
 const styles = {
+  //2xl represents minimum pixel width of 1536, if it is only make the screen 1280, else make it full
   innerWidth: '2xl:max-w-[1280px] w-full',
   interWidth: 'lg:w-[80%] w-[100%]',
 
   paddings: 'sm:p-16 xs:p-8 px-6 py-12',
   yPaddings: 'sm:py-16 xs:py-8 py-12',
+  //sm represents minimum pixel width and what to do when it exceeds that 640px
   xPaddings: 'sm:px-16 px-6',
   topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
   bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
