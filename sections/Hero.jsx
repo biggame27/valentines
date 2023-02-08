088996917823971ab07d08abb1b814b89b62dc7f
Harrison <koharrisonko@gmail.com>
2023-02-07 20:34:13 -0600

    finished title section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -32,6 +32,29 @@ const Hero = () => (
         </motion.div>
       </div>
     </motion.div>
+    <motion.div
+      variants={slideIn('right', 'tween', 0.2, 1)}
+      //margin top gives space, negative moves it up, relative makes it 
+      className="relative w-full md:-mt-[20px] -mt-[12px]"
+    >
+      <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]"/>
+      <img
+        src="/cover.png"
+        alt="cover"
+        className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-1 relative"
+      />
+
+      <a href="#explore">
+        <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+          <img
+            src='/stamp.png'
+            alt="stamp"
+            className="sm:w-[155px] w-[100px] sm:h[155px] h-[100px] object-contain"
+          />
+        </div>
+      </a>
+
+    </motion.div>
   </section>
 );
 
